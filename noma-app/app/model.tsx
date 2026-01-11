f2586b0809a900045bda4d49fb723d147131934f
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-11 07:21:07 +0100

    Metro config js

--- noma-app/app/model.tsx
+++ noma-app/app/model.tsx
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-import * as tf from '@tensorflow/tfjs';
-
 // Register backends
 import '@tensorflow/tfjs-backend-webgl';
 import '@tensorflow/tfjs-backend-cpu';
 import '@tensorflow/tfjs-react-native';
 
 import { Platform } from 'react-native';
+
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
+import { bundleResourceIO } from '@tensorflow/tfjs-react-native';
+import { Asset } from 'expo-asset';
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push('bin');
+
+module.exports = config;
+
 
 export const initTensorFlow = async () => {
   await tf.ready();
@@ -33,9 +44,43 @@ export const initTensorFlow = async () => {
 
 initTensorFlow();
 
+let model: tf.GraphModel | null = null;
+
+console.log(
+  require('../assets/tfjs_model/group1-shard1of1.bin')
+);
 
 
-const model = () => {
+// export const loadModel = async () => {
+//   if (model) return model;
+
+//   await tf.ready();
+//   // await tf.setBackend('cpu'); // safest on Android
+
+//   const modelJson = Asset.fromModule(
+//     require('../assets/tfjs_model/model.json')
+//   );
+
+//   const modelWeights = Asset.fromModule(
+//     require('../assets/tfjs_model/group1-shard1of1.bin')
+//   );
+
+//   await Promise.all([
+//     modelJson.downloadAsync(),
+//     modelWeights.downloadAsync(),
+//   ]);
+
+//   model = await tf.loadGraphModel(
+//     bundleResourceIO(modelJson, modelWeights)
+//   );
+
+//   console.log('✅ NomaApp GraphModel loaded');
+//   return model;
+// };
+
+
+
+const nomaModel = () => {
   return (
     <View>
       <Text>model</Text>
@@ -43,6 +88,6 @@ const model = () => {
   )
 }
 
-export default model
+export default nomaModel
 
 const styles = StyleSheet.create({})
